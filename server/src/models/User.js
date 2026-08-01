72e39d71dcb76953c40b5adae85eee883e27f554
Yupityr <jrchchrstngithub@gmail.com>
2026-08-01 19:51:47 +0800

    added hashing

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -25,6 +26,17 @@ const userSchema = new mongoose.Schema({
         lowercase:true
     }
 },{timestamps: true});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.has(this.password, salt);
+    next();
+})
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+}
 
 const User = mongoose.model("User", userSchema);
 
