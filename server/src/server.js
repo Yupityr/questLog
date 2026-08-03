769597e07452caf75ac21369dba57bdc29d8e3d5
Yupityr <jrchchrstngithub@gmail.com>
2026-08-03 20:24:46 +0800

    removing unnecessary files

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,6 @@ import 'dotenv/config';
 import express from 'express';
 import connectDB from './config/db.js';
 import cors from 'cors';
-import userRoutes from "./routes/userRoutes.js"
 import authRoutes from "./routes/authRoutes.js"
 import chapterRoutes from "./routes/chapterRoutes.js"
 import cookieParser from "cookie-parser";
@@ -12,7 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use("/users", userRoutes);
 app.use("/api/auth", authRoutes)
 app.use("/api/chapters", chapterRoutes);
 
