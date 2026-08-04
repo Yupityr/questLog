3f1ecf14d3cf74e1757333c4afcf5a2b804c44f2
Yupityr <jrchchrstngithub@gmail.com>
2026-08-04 19:44:14 +0800

    changed the way I import the fonts

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,11 @@ import { ChaptersProvider } from './context/ChaptersContext.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes.jsx'
+import '@fontsource/pixelify-sans/400.css'
+import '@fontsource/pixelify-sans/700.css'
+import '@fontsource/press-start-2p/400.css'
+import '@fontsource/silkscreen/400.css'
+import '@fontsource/silkscreen/700.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
