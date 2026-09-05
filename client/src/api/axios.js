c63d80aba2094129f3f39710a1b78a3f1d50b077
Yupityr <jrchchrstngithub@gmail.com>
2026-09-05 23:55:58 +0800

    api integration hotfix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.PROD
+const API_URL = import.meta.env.CLIENT_ENV
     ? import.meta.env.VITE_PROD_API
     : import.meta.env.VITE_DEV_API;
 
