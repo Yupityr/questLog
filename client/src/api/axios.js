8c1ee733c42d2187456c99c83145997cd8d896c6
Yupityr <jrchchrstngithub@gmail.com>
2026-09-05 23:52:17 +0800

    api handling fix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.PROD
+    ? import.meta.env.VITE_PROD_API
+    : import.meta.env.VITE_DEV_API;
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: API_URL,
     headers: {
         'Content-Type': 'application/json'
     },
