67244b3cb4546c7f26a3bc28f2d07423a9337d41
Yupityr <jrchchrstngithub@gmail.com>
2026-09-06 22:16:49 +0800

    Add: header nav functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,24 @@
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
+
     return (
         <header className="flex justify-between fixed top-0 left-0 w-full h-16  shadow-md z-50">
-            <div className="flex  items-center w-50">
+            <NavLink className="flex  items-center w-50" to="/">
                 <img src="/scroll.svg" alt="img" className="w-13"/>
                 QLogs
-            </div>
-            <div className="flex items-center">
+            </NavLink>
+            <div className="flex items-center mr-3">
                 <ul className="flex flex-row gap-5">
                     <li>
-                        Accomplishments
+                        <NavLink to="/quest">
+                            Accomplishments
+                        </NavLink>
                     </li>
                     <li>
-                        About
-                    </li>
-                    <li>
-                        
+                        <NavLink to="/about">
+                            About
+                        </NavLink>
                     </li>
                 </ul>
             </div>
